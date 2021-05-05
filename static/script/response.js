8aaafd58cef3b24dc5edc8d3faa2f8ef50c9405b
varungazula <you@example.com>
2021-05-05 22:36:37 +0530

    v2

---
+++ static/script/response.js
@@ -0,0 +1,33 @@
+$(".addtransaction").click(function(e){
+    var transaction = $("#transaction").val()
+    var mname = $("#mname").val()
+    var bname = $("#bname").val()
+    var type1 = $("#type1").val()
+    var date = $("#date").val()
+    
+    
+    if (transaction.length>0 && mname.length>0 && bname.length>0 && type1.length>0 && date.length>0){
+        e.preventDefault()
+        Swal.fire({
+            title: 'Do you want to add this transaction?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: `Add`,
+            denyButtonText: `Don't Add`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+    
+            $.post("/transactionadd", {transaction:transaction, mname:mname, bname:bname,type1:type1, date:date}, function(data){})
+                
+              Swal.fire('Added!', '', 'success')
+              setTimeout(function(){  location.href = "transactions"; }, 2000);
+            } else if (result.isDenied) {
+            // $("#modal1").removeClass("is-active");
+    
+              Swal.fire('Changes are not saved', '', 'info')
+    
+            }
+          })
+    }
+    })
